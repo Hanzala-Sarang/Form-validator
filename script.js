18ab3bf35fcbe4756a937b3566539df37adfa513
Hanzala-Sarang <Hanzalasarang01@gmail.com>
2023-12-09 20:03:09 +0530

    form-validator app

---
+++ script.js
@@ -0,0 +1,83 @@
+const nameError = document.getElementById('name-error');
+const phoneError = document.getElementById('phone-error');
+const emailError = document.getElementById('email-error');
+const msgError = document.getElementById('message-error');
+const submitError = document.getElementById('submit-error');
+
+
+function validateName(){
+    const name = document.getElementById('contact-name').value;
+
+    if (name.length == 0){
+        nameError.innerHTML = '*Name is required';
+        return false;
+    
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Enter full name';
+        return false;
+ 
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true
+ 
+}
+
+function validatePhone(){
+    const phone = document.getElementById('contact-phone').value;
+
+    if(phone.length == 0){
+        phoneError.innerHTML = '*Phone no is required';
+        return false;
+    }
+    if(phone.length !== 10){
+        phoneError.innerHTML = 'Phone no should be 10 digits';
+        return false
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'Only digits are required';
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+    return true;
+}
+
+function validateEmail(){
+    const email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = '*Email is required'
+        return false
+    }
+    if(!email.match(/^[A-za-z\.\_\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,5}$/)){
+        emailError.innerHTML = 'Invalid Email'
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+    return true;
+}
+
+function validateMessage(){
+    const message = document.getElementById('contact-message').value;
+    const required = 30;
+    let left = required - message.length;
+
+    if (left > 0 ){
+        msgError.innerHTML = `${left} more characters required`
+        return false
+    }
+    msgError.innerHTML =  '<i class="fa-solid fa-circle-check"></i>'
+    return true;
+}
+
+function validateForm(){
+
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+        submitError.style.display = 'block'
+        submitError.innerHTML = 'Please fill the required details'
+        setTimeout(()=>{submitError.style.display = 'none'},3000)
+        return false;
+    }
+    return true
+}
